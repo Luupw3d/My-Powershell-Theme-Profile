368549310fd6e28802baa0a03b627f04782e7b80
Luupw3d <wildl0tuso@gmail.com>
2023-06-14 21:59:39 +0800

    Update Microsoft.PowerShell_profile.ps1
    适配oh-my-posh3.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,7 @@
-Import-Module posh-git 
-Import-Module oh-my-posh
-Import-Module DirColors
-#Import-Module PoshFunctions 
-Set-PoshPrompt half-life-mod
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\half-life-mod.omp.json" | Invoke-Expression
+Import-Module posh-git
+Import-Module Terminal-Icons
+#Import-Module DirColors
 Enable-PoshTooltips
 
 #-------------------------------  Set Hot-keys BEGIN  -------------------------------
@@ -28,48 +27,8 @@ Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 #-------------------------------  Set Hot-keys END    -------------------------------
 
-#-------------------------------    Functions BEGIN   -------------------------------
-# Python 直接执行
-$env:PATHEXT += ";.py"
-
-# 更新系统组件
-function Update-Packages {
-	# update pip
-	Write-Host "Step 1: 更新 pip" -ForegroundColor Magenta -BackgroundColor Cyan
-	$a = pip list --outdated
-	$num_package = $a.Length - 2
-	for ($i = 0; $i -lt $num_package; $i++) {
-		$tmp = ($a[2 + $i].Split(" "))[0]
-		pip install -U $tmp
-	}
-
-	# update TeX Live
-	$CurrentYear = Get-Date -Format yyyy
-	Write-Host "Step 2: 更新 TeX Live" $CurrentYear -ForegroundColor Magenta -BackgroundColor Cyan
-	tlmgr update --self
-	tlmgr update --all
-
-	# update Chocolotey
-	Write-Host "Step 3: 更新 Chocolatey" -ForegroundColor Magenta -BackgroundColor Cyan
-	choco outdated
-}
-#-------------------------------    Functions END     -------------------------------
-
-
-
-
-
 #-------------------------------   Set Alias BEGIN    -------------------------------
-# 1. 编译函数 make
-function MakeThings {
-	nmake.exe $args -nologo
-}
-Set-Alias -Name make -Value MakeThings
-
-# 2. 更新系统 os-update
-Set-Alias -Name os-update -Value Update-Packages
-
-# 3. 查看目录 ls & ll
+# 1. 查看目录 ls & ll
 function ListDirectory {
 	(Get-ChildItem).Name
 	Write-Host("")
@@ -80,7 +39,7 @@ Remove-Item alias:\ls
 Set-Alias -Name ls -Value Get-ChildItemColorFormatWide
 Set-Alias -Name ll -Value Get-ChildItemColor
 
-# 4. 打开当前工作目录
+# 2. 打开当前工作目录
 function OpenCurrentFolder {
 	param
 	(
@@ -93,27 +52,3 @@ function OpenCurrentFolder {
 }
 Set-Alias -Name open -Value OpenCurrentFolder
 #-------------------------------    Set Alias END     -------------------------------
-
-
-#-------------------------------   Set Network BEGIN    -------------------------------
-# 1. 获取所有 Network Interface
-function Get-AllNic {
-	Get-NetAdapter | Sort-Object -Property MacAddress
-}
-Set-Alias -Name getnic -Value Get-AllNic
-
-# 2. 获取 IPv4 关键路由
-function Get-IPv4Routes {
-	Get-NetRoute -AddressFamily IPv4 | Where-Object -FilterScript {$_.NextHop -ne '0.0.0.0'}
-}
-Set-Alias -Name getip -Value Get-IPv4Routes
-
-# 3. 获取 IPv6 关键路由
-function Get-IPv6Routes {
-	Get-NetRoute -AddressFamily IPv6 | Where-Object -FilterScript {$_.NextHop -ne '::'}
-}
-Set-Alias -Name getip6 -Value Get-IPv6Routes
-#-------------------------------    Set Network END     -------------------------------
-
-
-
